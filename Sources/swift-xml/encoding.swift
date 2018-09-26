b5ba21cc6640ca4ee3c95d74c5c55e708403dc56
bjtj <bjtj10@gmail.com>
2018-09-26 21:21:07 +0900

    access modifier

--- Sources/swift-xml/encoding.swift
+++ Sources/swift-xml/encoding.swift
@@ -1,12 +1,12 @@
 import Foundation
 
-func escapeXml(text: String) -> String {
+public func escapeXml(text: String) -> String {
     return text.replacingOccurrences(of: "&", with: "&amp;")
       .replacingOccurrences(of: "<", with: "&gt;")
       .replacingOccurrences(of: ">", with: "&lt;")
 }
 
-func unescapeXml(text: String) -> String {
+public func unescapeXml(text: String) -> String {
     return text.replacingOccurrences(of: "&gt;", with: "<")
       .replacingOccurrences(of: "&lt;", with: ">")
       .replacingOccurrences(of: "&amp;", with: "&")
